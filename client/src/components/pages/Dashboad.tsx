8f75a79cc4128cada37c68e92d0ea06a3eb92764
shinyamizuno1008 <bitsukun75@gmail.com>
2019-05-03 13:19:25 +0900

    add getting state from global in dashboard

--- client/src/components/pages/Dashboad.tsx
+++ client/src/components/pages/Dashboad.tsx
@@ -1,9 +1,17 @@
 import * as React from 'react';
+import { Subscribe } from 'unstated-typescript';
+import TargetContainer from '../../containers/TargetContainer';
 
 interface Props {}
 
 const component: React.FC<Props> = () => {
-  return <h1>This is the dashboard</h1>;
+  return (
+    <Subscribe to={[TargetContainer]}>
+      {target => (
+        <h1>This is the dashboard and target is {target.state.target}</h1>
+      )}
+    </Subscribe>
+  );
 };
 
 export default component;
