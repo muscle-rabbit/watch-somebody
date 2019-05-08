a3cf0b3d7b5ce26ac5480596882a6d9c5b5e96b8
shinyamizuno1008 <bitsukun75@gmail.com>
2019-05-09 06:34:44 +0900

    add getting tweet

--- server/client.go
+++ server/client.go
@@ -1,14 +1,49 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
+
+	"github.com/ChimeraCoder/anaconda"
 )
 
-type client struct {
-	http.Client
-	userData map[string]interface{}
+type Client struct {
+	// http.Client
+	TwitterAPI *anaconda.TwitterApi
+	UserData   map[string]string
 }
 
-func GetClient(target string) client {
-	return &client{userData["target"]: target}
+func (*Client) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/dashboard" {
+		http.Error(w, "404 not found.", http.StatusNotFound)
+		return
+	}
+	switch r.Method {
+	case "POST":
+		if err := r.ParseForm(); err != nil {
+			fmt.Fprintf(w, "ParseForm() err: %v", err)
+			return
+		}
+		var v map[string]string
+		decoder := json.NewDecoder(r.Body)
+		err := decoder.Decode(&v)
+		if err != nil {
+			log.Fatal("decoder error")
+			panic(err)
+		}
+		// client.UserData["target"] = v["target"]
+		// log.Println(v["target"])
+		searchResult, err := client.TwitterAPI.GetSearch(v["target"], nil)
+		if err != nil {
+			log.Fatal("twitter eror")
+			panic(err)
+		}
+		for _, tweet := range searchResult.Statuses {
+			log.Println(tweet)
+		}
+
+	}
+	fmt.Fprintf(w, "Sorry, only POST methods are supported.")
 }
--- server/main.go
+++ server/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/ChimeraCoder/anaconda"
 )
 
-func echo(w http.ResponseWriter, r *http.Request) {
+var client Client
+
+func postHandler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/dashboard" {
 		http.Error(w, "404 not found.", http.StatusNotFound)
 		return
@@ -17,19 +22,25 @@ func echo(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprintf(w, "ParseForm() err: %v", err)
 			return
 		}
-		fmt.Fprintf(w, "Post from website! r.PostFrom = %v\n", r.PostForm)
-		name := r.FormValue("name")
-		address := r.FormValue("address")
-		fmt.Fprintf(w, "Name = %s\n", name)
-		fmt.Fprintf(w, "Address = %s\n", address)
+		var v map[string]string
+		decoder := json.NewDecoder(r.Body)
+		err := decoder.Decode(&v)
+		if err != nil {
+			panic(err)
+		}
 	}
 	fmt.Fprintf(w, "Sorry, only POST methods are supported.")
 }
 
 func main() {
 	fs := http.FileServer(http.Dir("../dist"))
+	api := anaconda.NewTwitterApiWithCredentials("973805197523906560-p5UX08QTzbYsIlbudD9MlqL7GyYaoal", "YDNsHJ8dpDXIEUAQotqH4SsE6wwlGt0V2GFbDZKKToM7f", "q7sRsAMlYGfFubENPogmgM4S0", "s3wUvTHeVJAiC8fjYCgDALi2yegrNd3ZvakXe5zGjUO3uKdXWY")
+
+	client = Client{TwitterAPI: api}
 	http.Handle("/", fs)
-	http.HandleFunc("/dashboard", echo)
+	http.Handle("/dashboard", &client)
+	// http.HandleFunc("/dashboard", postHandler)
+
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
